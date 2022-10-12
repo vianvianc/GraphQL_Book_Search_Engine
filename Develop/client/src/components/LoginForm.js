deabf2008508e0d0ca641cb78ac65431eff853a7
Vian Charbonneau <102205118+vianvianc@users.noreply.github.com>
2022-10-12 02:34:55 +0200

    testing

--- Develop/client/src/components/LoginForm.js
+++ Develop/client/src/components/LoginForm.js
@@ -10,6 +10,7 @@ const LoginForm = () => {
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
 
+  
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
